b4cf8c9f679002665eceb9fcf67b5b27813b2acd
xXQuantumSoulXx <killinggamer4mc@gmail.com>
2022-12-30 22:48:10 +0000

    Redo the logic

--- src/commands/rps.rs
+++ src/commands/rps.rs
@@ -1,8 +1,8 @@
 use rand::Rng;
-use serenity::builder::CreateApplicationCommand;
+use serenity::builder::{CreateApplicationCommand, CreateEmbed};
 use serenity::client::Context;
-use serenity::model::interactions::application_command::ApplicationCommandOptionType;
-use serenity::model::prelude::application_command::ApplicationCommandInteraction;
+use serenity::model::application::command::CommandOptionType;
+use serenity::model::prelude::interaction::application_command::ApplicationCommandInteraction;
 use crate::OPTIONS;
 use crate::util::reply_cmd_interaction::reply_cmd_interaction;
 
@@ -14,7 +14,7 @@ pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicatio
             o
                 .name("choice")
                 .description("Pick rock, paper, or scissor")
-                .kind(ApplicationCommandOptionType::String)
+                .kind(CommandOptionType::String)
                 .required(true)
                 .add_string_choice("rock", "rock")
                 .add_string_choice("paper", "paper")
@@ -31,86 +31,51 @@ pub async fn run(ctx: Context, interaction: ApplicationCommandInteraction) {
         Some(ref value) => value.as_str().unwrap(),
         None => "invalid",
     };
+    let user_outcome_index = OPTIONS.iter().position(|&r| r == user_outcome);
 
     println!("User outcome: {}", user_outcome);
 
-    if !OPTIONS.contains(&user_outcome) {
-
+    if user_outcome_index.is_none() {
         reply_cmd_interaction(ctx, interaction, true, vec![], Some("Invalid Input"))
             .await
             .expect("Could not send RPS result message");
         return;
     }
 
-    if bot_outcome.to_string() == user_outcome.to_string() {
-        reply_cmd_interaction(ctx, interaction, true, vec![], Some("It's a tie!"))
-            .await
-            .expect("Could not send RPS result message");
-        return;
+    let user_outcome_index = user_outcome_index.unwrap();
+
+    let result: &str;
+
+    if (user_outcome_index + 1) % 3 == bot_outcome_index {
+        // Bot won
+        result = "I won!";
+    } else if (bot_outcome_index + 1) % 3 == user_outcome_index {
+        // Player won
+        result = "You won!";
+    } else {
+        // Tie
+        result = "We drew!";
     }
 
-    // Match comments are formatted as "bot_outcome vs user_outcome"
-    match bot_outcome.as_str() {
-        "rock" => {
-            match user_outcome {
-                "paper" => {
-                    // rock vs paper
-                    reply_cmd_interaction(ctx, interaction, true, vec![], Some("I got rock. I lose!"))
-                        .await
-                        .expect("Could not send RPS result message");
-                }
-                "scissor" => {
-                    // rock vs scissor
-                    reply_cmd_interaction(ctx, interaction, true, vec![], Some("I got rock. I win!"))
-                        .await
-                        .expect("Could not send RPS result message");
-                }
-                _ => tie(ctx, interaction, bot_outcome).await
-            }
-        }
-        "paper" => {
-            match user_outcome {
-                "rock" => {
-                    // paper vs rock
-                    reply_cmd_interaction(ctx, interaction, true, vec![], Some("I got paper. I win!"))
-                        .await
-                        .expect("Could not send RPS result message");
-                }
-                "scissor" => {
-                    // paper vs scissor
-                    reply_cmd_interaction(ctx, interaction, true, vec![], Some("I got paper. I lose!"))
-                        .await
-                        .expect("Could not send RPS result message");
-                }
-                _ => tie(ctx, interaction, bot_outcome).await
-            }
-        }
-        "scissors" => {
-            match user_outcome {
-                "paper" => {
-                    // scissors vs paper
-                    reply_cmd_interaction(ctx, interaction, true, vec![], Some("I got scissors. I win!"))
-                        .await
-                        .expect("Could not send rock paper scissor result message");
-                }
-                "rock" => {
-                    // scissors vs rock
-                    reply_cmd_interaction(ctx, interaction, true, vec![], Some("I got scissors. I lose!"))
-                        .await
-                        .expect("Could not send rock paper scissor result message");
-                }
-                _ => tie(ctx, interaction, bot_outcome).await
-            }
-        }
-        _ => {}
-    }
-}
-
-async fn tie(ctx: Context, interaction: ApplicationCommandInteraction, bot_outcome: &str) {
-    match reply_cmd_interaction(ctx, interaction, true, vec![], Some(&*format!("I got {}. We drew!", bot_outcome))).await {
-        Ok(_) => {}
-        Err(e) => {
-            println!("Error sending tie message: {}", e);
-        }
-    };
+    reply_cmd_interaction(
+        ctx,
+        interaction.to_owned(),
+        true,
+        vec![
+            CreateEmbed::default()
+                .title("Rock Paper Scissors")
+                .description(
+                    format!("You: {}\n\
+                    Me: {}\n\
+                    {}",
+                            user_outcome,
+                            bot_outcome,
+                            result
+                    )
+                )
+                .to_owned()
+        ],
+        None)
+        .await
+        .expect("Could not send RPS result message");
 }
